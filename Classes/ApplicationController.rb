dff7725e470b888e5db70d774b92511ba000b455
Burke Libbey <burke@burkelibbey.org>
2011-10-08 19:29:38 -0500

    initial

---
+++ Classes/ApplicationController.rb
@@ -0,0 +1,65 @@
+require 'open-uri'
+require 'json'
+
+class ApplicationController
+  KInternetEventClass = KAEGetURL = 'GURL'.unpack('N').first
+  KeyDirectObject = '----'.unpack('N').first
+
+  attr_accessor :menu
+
+  def awakeFromNib
+    @status_bar = NSStatusBar.systemStatusBar
+    @status_item = @status_bar.statusItemWithLength(NSVariableStatusItemLength)
+    @status_item.setHighlightMode(true)
+    @status_item.setMenu(@menu)
+
+    @app_empty_icon = NSImage.imageNamed('skullspace-empty.png')
+    @app_empty_alter_icon = NSImage.imageNamed('skullspace-empty-alt.png')
+    @app_full_icon = NSImage.imageNamed('skullspace-full.png')
+    @app_full_alter_icon = NSImage.imageNamed('skullspace-full-alt.png')
+    @app_problem_icon = NSImage.imageNamed('skullspace-problem.png')
+    @app_problem_alter_icon = NSImage.imageNamed('skullspace-problem-alt.png')
+
+    
+    @status_item.setImage(@app_problem_icon)
+    @status_item.setAlternateImage(@app_problem_alter_icon)
+    
+    Thread.new do
+      loop do
+        updateStatus
+        sleep 10
+      end      
+    end
+  end
+
+  def updateStatus
+    begin
+      case open("http://isthereanyoneatskullspace.com/index.txt").read
+      when /YES/
+        setFull
+      when /NO/
+        setEmpty
+      else
+        setProblem
+      end
+    rescue 
+      setProblem
+    end
+  end
+  
+  def setProblem
+    @status_item.setImage(@app_problem_icon)
+    @status_item.setAlternateImage(@app_problem_alter_icon)
+  end
+  
+  def setFull
+    @status_item.setImage(@app_full_icon)
+    @status_item.setAlternateImage(@app_full_alter_icon)
+  end
+  
+  def setEmpty
+    @status_item.setImage(@app_empty_icon)
+    @status_item.setAlternateImage(@app_empty_alter_icon)
+  end 
+    
+end
